c48cf6df9be52db7a4e8ed9fcad0fd4be373f977
miyahara <50367145+subretu@users.noreply.github.com>
2022-04-25 23:27:22 +0900

    ダッシュボード作成

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <template>
   <v-app>
-    <v-card width="400px" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">ログイン</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
-          <v-text-field
-            v-bind:type="showPassword ? 'text' : 'password'"
-            @click:append="showPassword = !showPassword"
-            prepend-icon="mdi-lock"
-            v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            label="パスワード"
-            v-model="password"
-          />
-          <v-card-actions>
-            <v-btn>ログイン</v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card-text>
-    </v-card>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      Navigation Lists
+    </v-navigation-drawer>
+    <v-app-bar color="primary" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-app-bar>
+    <v-footer color="primary" dark app> Vuetify </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data: () => ({
-    showPassword: false,
-  }),
+  data() {
+    return {
+      drawer: null,
+    };
+  },
 };
 </script>
