4ea973cd5be652e1f640a6351b09fe4e97bb2f89
miyahara <50367145+subretu@users.noreply.github.com>
2022-04-28 23:30:05 +0900

    TOPページに画像を表示

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <h1>TopPage</h1>
-  </div>
+  <section class="home-hero">
+    <v-container fluid fill-height class="home-hero__content">
+      <v-row>
+        <v-col class="home-hero__content-text"> </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
+<style lang="scss" scoped>
+.home-hero__content {
+  background: url("../assets/img/text_youkoso.png");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 80vh;
+
+  &-text {
+    color: white;
+    text-align: center;
+    font-size: 28px;
+    font-weight: bold;
+  }
+}
+</style>
