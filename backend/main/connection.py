e75ba70037bb1abfbbae2ddaa823733defb16fb0
miyahara <50367145+subretu@users.noreply.github.com>
2022-04-29 23:38:37 +0900

    不要部分を削除

--- backend/main/connection.py
+++ backend/main/connection.py
@@ -3,13 +3,6 @@ import psycopg2
 
 # DB接続用関数
 def get_connection():
-    """
-    user = os.getenv("POSTGRES_USER", None)
-    pwd = os.getenv("POSTGRES_PASS", None)
-    server = os.getenv("POSTGRES_HOST", None)
-    port = os.getenv("POSTGRES_HOST", None)
-    db = os.getenv("POSTGRES_PORT", None)
-    """
     user = "postgres"
     pwd = "postgres"
     server = "127.0.0.1"
