cdabdaeddc6e8a819b7240695441a793475e88b2
miyahara <50367145+subretu@users.noreply.github.com>
2022-05-15 22:07:18 +0900

    Responsive Designを設定

--- frontend/src/views/ChartSample.vue
+++ frontend/src/views/ChartSample.vue
@@ -2,16 +2,16 @@
   <v-container>
     <h3>ChartSample</h3>
     <v-row>
-      <v-col cols="6">
+      <v-col cols="12" sm="12" md="6" lg="6">
         <line-chart :chart-data="datacollection" :options="options" />
       </v-col>
-      <v-col cols="6">
+      <v-col cols="12" sm="12" md="6" lg="6">
         <bar-chart :chart-data="datacollection" :options="options" />
       </v-col>
-      <v-col cols="6">
+      <v-col cols="12" sm="12" md="6" lg="6">
         <line-chart :chart-data="datacollection" :options="options" />
       </v-col>
-      <v-col cols="6">
+      <v-col cols="12" sm="12" md="6" lg="6">
         <bar-chart :chart-data="datacollection" :options="options" />
       </v-col>
     </v-row>
