9d5ef930a86a0d44afa9e8ef752ecf5c4469b921
miyahara <50367145+subretu@users.noreply.github.com>
2022-07-20 23:49:48 +0900

    add Composition API

--- frontend/src/views/UserList.vue
+++ frontend/src/views/UserList.vue
@@ -11,58 +11,62 @@
   </v-container>
 </template>
 
-<script>
-export default {
-  name: "UserList",
-  data() {
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const headers = [
+      {
+        text: "ID",
+        value: "id",
+      },
+      {
+        text: "名前",
+        value: "name",
+      },
+      {
+        text: "年齢",
+        value: "age",
+      },
+    ];
+    const items = [
+      {
+        id: 1,
+        name: "高田健志",
+        age: 33,
+      },
+      {
+        id: 2,
+        name: "横山緑",
+        age: 42,
+      },
+      {
+        id: 3,
+        name: "山田太郎",
+        age: 10,
+      },
+      {
+        id: 4,
+        name: "山崎イチゴ",
+        age: 12,
+      },
+      {
+        id: 5,
+        name: "山田ニゴ",
+        age: 16,
+      },
+      {
+        id: 6,
+        name: "山本一之進",
+        age: 12,
+      },
+    ];
+
     return {
-      headers: [
-        {
-          text: "ID",
-          value: "id",
-        },
-        {
-          text: "名前",
-          value: "name",
-        },
-        {
-          text: "年齢",
-          value: "age",
-        },
-      ],
-      items: [
-        {
-          id: 1,
-          name: "高田健志",
-          age: 33,
-        },
-        {
-          id: 2,
-          name: "横山緑",
-          age: 42,
-        },
-        {
-          id: 3,
-          name: "山田太郎",
-          age: 10,
-        },
-        {
-          id: 4,
-          name: "山崎イチゴ",
-          age: 12,
-        },
-        {
-          id: 5,
-          name: "山田ニゴ",
-          age: 16,
-        },
-        {
-          id: 6,
-          name: "山本一之進",
-          age: 12,
-        },
-      ],
+      headers,
+      items,
     };
   },
-};
+});
 </script>
