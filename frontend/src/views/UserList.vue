2112ce5bf13b77034c242b87d93528111999a76d
miyahara <50367145+subretu@users.noreply.github.com>
2022-12-16 23:59:03 +0900

    add data length display

--- frontend/src/views/UserList.vue
+++ frontend/src/views/UserList.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <h3>UserList</h3>
+    {{ items.length }}件
     <v-row>
       <v-col cols="10">
         <v-sheet color="white" elevation="1">
@@ -42,11 +43,6 @@ export default defineComponent({
         age: 42,
       },
       {
-        id: 3,
-        name: "山田太郎",
-        age: 10,
-      },
-      {
         id: 4,
         name: "山崎イチゴ",
         age: 12,
@@ -62,7 +58,6 @@ export default defineComponent({
         age: 12,
       },
     ];
-
     return {
       headers,
       items,
