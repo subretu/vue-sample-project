5235b5ee3c71ab457b1752c7fa8cb306fd90861e
miyahara <50367145+subretu@users.noreply.github.com>
2023-09-12 00:33:41 +0900

    Add dialog  open button

--- frontend/src/views/AxiosTest2.vue
+++ frontend/src/views/AxiosTest2.vue
@@ -40,8 +40,50 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-dialog v-model="dialog2" width="500">
+      <v-card>
+        <v-card-title class="headline"
+          >データ詳細
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon class="ml-1" color="black" dark v-bind="attrs" v-on="on">
+                mdi-help-circle-outline
+              </v-icon>
+            </template>
+            <span>これは選択されたデータの詳細を表示している画面です</span>
+          </v-tooltip></v-card-title
+        >
+        <v-select
+          :items="viewSelectBoxData"
+          outlined
+          dense
+          class="ml-6"
+          @change="getTargetData"
+        ></v-select>
+        <v-card-text>
+          <v-row>
+            <v-col cols="3">ID</v-col
+            ><v-col cols="9">{{ dialogData2.id }}</v-col></v-row
+          >
+          <v-divider />
+          <v-row>
+            <v-col cols="3">日付</v-col
+            ><v-col cols="9">{{ dialogData2.label }}</v-col></v-row
+          ><v-divider />
+          <v-row>
+            <v-col cols="3">合計値</v-col
+            ><v-col cols="9">{{ dialogData2.data }}</v-col></v-row
+          >
+        </v-card-text>
+        <v-card-actions class="pa-6">
+          <v-spacer />
+          <v-btn @click="closeDialog2">Close Dialog</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-row>
-      <v-col cols="10">
+      <v-col cols="10"
+        ><v-btn class="mb-2" @click="openDialog2">モーダルを開く</v-btn>
         <v-sheet color="white" elevation="1">
           <v-data-table
             :headers="headers"
@@ -69,12 +111,13 @@ import {
   ref,
   computed,
   onMounted,
+  watch,
 } from "@vue/composition-api";
 
 type DataList = {
-  id?: string | null;
-  label?: string | null;
-  data?: string | null;
+  id?: string | null | undefined;
+  label?: string | null | undefined;
+  data?: number | null | undefined;
 };
 
 type ApiResponse = { data: DataList[] };
@@ -142,17 +185,64 @@ export default defineComponent({
       }
     };
 
+    const dialog2 = ref(false);
+
+    const dialogData2: DataList = reactive({
+      id: "",
+      label: "",
+      data: 0,
+    });
+
+    const openDialog2 = () => {
+      dialog2.value = true;
+    };
+
+    const closeDialog2 = (): void => {
+      dialog2.value = false;
+    };
+
+    const getTargetData = (event: any) => {
+      const eventList = event.split("_");
+      apiResponse.data.forEach(function (element) {
+        if (element.id === eventList[0]) {
+          dialogData2.id = element.id;
+          dialogData2.label = element.label;
+          dialogData2.data = element.data;
+          return;
+        }
+      });
+    };
+
+    // モーダルの開閉を監視
+    watch(
+      () => dialog2.value,
+      async () => {
+        if (dialog2.value) {
+          await selectBoxData();
+        } else {
+          dialogData2.id = "";
+          dialogData2.label = "";
+          dialogData2.data = 0;
+          viewSelectBoxData.value = [];
+        }
+      }
+    );
+
     onMounted(async () => {
       await displayData();
-      await selectBoxData();
     });
 
     return {
       headers,
       dialog,
+      dialog2,
       dialogData,
+      dialogData2,
       openDialog,
+      openDialog2,
+      getTargetData,
       closeDialog,
+      closeDialog2,
       viewData,
       viewSelectBoxData,
     };
